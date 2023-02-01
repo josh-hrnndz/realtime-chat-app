818ec9d6e12870da2c7706f9888d0859d518ee57
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-01 11:44:43 +0800

    feat(core): :sparkles: add string constants used in the app

--- lib/core/const/strings.dart
+++ lib/core/const/strings.dart
@@ -1,3 +1,6 @@
 // ignore_for_file: non_constant_identifier_names
 
 String TALK_TO_STRANGER = "Talk to a Stranger!";
+String NEXT = "NEXT";
+String STOP = "STOP";
+String CHATBOX_HINTTEXT = "Send a message...";
