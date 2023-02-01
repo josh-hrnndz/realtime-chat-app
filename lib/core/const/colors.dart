46e52525e30724f55c9eb8b37827e27fc5f714b4
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-01 11:42:59 +0800

    feat(core): :sparkles: add color constant

--- lib/core/const/colors.dart
+++ lib/core/const/colors.dart
@@ -6,10 +6,11 @@ const Color PRIMARY = Color(0xFF8CC7C8);
 const Color DARK = Color(0xFF3D5254);
 const Color SECONDARY = Color(0xFFD9D9D9);
 const Color ACCENT = Color(0xFFFca3de);
-const Color RED = Color(0xFFEF8282);
 
+const Color TINT_RED = Color(0xFFEF8282);
 const Color TINT_GREEN = Color(0xFFCBF09C);
 const Color PRIMARY_DARK = Color(0xFF6C9BA0);
+const Color PRIMARY_LIGHT = Color(0xFFA4ECFC);
 
 const Color TEXT_DARK = Color(0xFF111111);
 const Color TEXT_PRIMARY = Color(0xFF121212);
