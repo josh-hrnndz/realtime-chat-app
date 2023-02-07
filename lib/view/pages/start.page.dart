00d775f5307628f4a1fb537bbf632e8004a52b82
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-07 14:54:59 +0800

    feat(page): :sparkles: add connect function to start page button

--- lib/view/pages/start.page.dart
+++ lib/view/pages/start.page.dart
@@ -10,34 +10,7 @@ class StartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            SizedBox(
-              height: height * .4,
-              width: width * .8,
-              child: Image.asset(
-                'assets/icon.png',
-              ),
-            ),
-            SizedBox(
-              height: height * .07,
-              width: width * .5,
-              child: AppButton(
-                buttonType: 'text',
-                textLabel: TALK_TO_STRANGER,
-                fontSize: 15,
-                fontWeight: FontWeight.w600,
-                buttonColor: DARK,
-                borderRadius: 21,
-                onClick: () {
-                  context.goNamed(CHAT);
-                },
-              ),
-            ),
-          ],
+                    BlocProvider.of<SocketCubit>(context).connect();
         ),
       ),
     );
