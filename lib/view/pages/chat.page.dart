5a1e35736461549f75bb0f897b7c7d53d3ffae86
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-08 17:05:03 +0800

    feat(page): :sparkles: created chat page functionality

--- lib/view/pages/chat.page.dart
+++ lib/view/pages/chat.page.dart
@@ -1,98 +1,137 @@
 import 'package:flutter/material.dart';
-import 'package:realtime_chat_app/core/const/colors.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:realtime_chat_app/core/const/index.dart';
+import 'package:realtime_chat_app/core/di.dart';
+import 'package:realtime_chat_app/view/bloc/cubit/socket_cubit.dart';
 import 'package:realtime_chat_app/view/widgets/button.widget.dart';
 import 'package:realtime_chat_app/view/widgets/chat.widget.dart';
 import 'package:realtime_chat_app/view/widgets/chatbox.widget.dart';
 import 'package:realtime_chat_app/view/widgets/control.button.widget.dart';
 
+import '../../data/model/response.model.dart';
+
 class ChatPage extends StatelessWidget {
   ChatPage({super.key});
+  
+  final chatBoxController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+  String userId = "";
+  List<Response> responses = [];
 
-  final chat = TextEditingController();
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
-
     bool isUser = false;
+    
 
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Row(
-            children: [
-              ControlButton(
-                height: height * .08,
-                width: width * .5,
-                buttonColor: TINT_GREEN,
-                label: NEXT,
-                onTap: () {},
-              ),
-              ControlButton(
-                height: height * .08,
-                width: width * .5,
-                buttonColor: TINT_RED,
-                label: STOP,
-                onTap: () {},
-              ),
-            ],
-          ),
-          Expanded(
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * .03),
-              child: ListView.builder(
-                reverse: true,
-                itemCount: 20,
-                itemBuilder: (BuildContext context, int index) {
-                  index % 2 == 0 ? isUser = true : isUser = false;
-                  return Padding(
-                    padding: EdgeInsets.symmetric(vertical: height * .005),
-                    child: Align(
-                      alignment:
-                          isUser ? Alignment.centerRight : Alignment.centerLeft,
-                      child: Chat(
-                        isUser: isUser,
-                        message:
-                            "Hello Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vel est sed magna laoreet tristique vitae ac arcu. Aenean laoreet neque elit, non volutpat turpis consequat nec. Mauris at mi sit amet metus cursus sodales. ${20 - 1 - index}",
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: height * .02),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return BlocProvider(
+      create: (context) => sl<SocketCubit>(),
+      child: BlocConsumer<SocketCubit, SocketState>(
+        listener: (context, state) {
+          if (state is ReceiveMessagesState) {
+            userId = state.userId;
+            responses = state.responses;
+          }
+        },
+        builder: (context, state) {
+          if (state is SocketInitial) {
+            BlocProvider.of<SocketCubit>(context).getMessages();
+          }
+          return Scaffold(
+            body: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                SizedBox(
-                  width: width * .75,
-                  child: ChatBox(
-                    textEditingController: chat,
-                    hintText: CHATBOX_HINTTEXT,
+                Row(
+                  children: [
+                    ControlButton(
+                      height: height * .08,
+                      width: width * .5,
+                      buttonColor: TINT_GREEN,
+                      label: NEXT,
+                      onTap: () {},
+                    ),
+                    ControlButton(
+                      height: height * .08,
+                      width: width * .5,
+                      buttonColor: TINT_RED,
+                      label: STOP,
+                      onTap: () {},
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: width * .03),
+                    child: ListView.builder(
+                      controller: _scrollController,
+                      itemCount: responses.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        if (index == responses.length - 1) {
+                          _scrollToBottom();
+                        }
+                        responses[index].userId == userId
+                            ? isUser = true
+                            : isUser = false;
+                        return Padding(
+                          padding:
+                              EdgeInsets.symmetric(vertical: height * .005),
+                          child: Align(
+                            alignment: isUser
+                                ? Alignment.centerRight
+                                : Alignment.centerLeft,
+                            child: Chat(
+                              isUser: isUser,
+                              message: responses[index].message,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
                 ),
-                SizedBox(
-                  width: width * .13,
-                  height: height * .07,
-                  child: AppButton(
-                    buttonType: "icon",
-                    icon: Icons.send,
-                    isIconRotated: true,
-                    iconRotation: 320,
-                    fontSize: 25,
-                    borderRadius: 25,
-                    onClick: () {},
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: height * .02),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SizedBox(
+                        width: width * .75,
+                        child: ChatBox(
+                          textEditingController: chatBoxController,
+                          hintText: CHATBOX_HINTTEXT,
+                        ),
+                      ),
+                      SizedBox(
+                        width: width * .13,
+                        height: height * .07,
+                        child: AppButton(
+                          buttonType: "icon",
+                          icon: Icons.send,
+                          isIconRotated: true,
+                          iconRotation: 320,
+                          fontSize: 25,
+                          borderRadius: 25,
+                          onClick: () {
+                            BlocProvider.of<SocketCubit>(context)
+                                .sendMessage(userId, chatBoxController.text);
+                            chatBoxController.text = "";
+                          },
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
+  }
+
+  void _scrollToBottom() {
+    _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeOut);
   }
 }
