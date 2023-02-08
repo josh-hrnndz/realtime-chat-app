c3b4ff0d50982606ddb343052d695fb1ffe278ce
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-08 17:03:22 +0800

    feat(repository): :sparkles: create send, get message and getters and setters for repository

--- lib/data/repository/socket.repository.impl.dart
+++ lib/data/repository/socket.repository.impl.dart
@@ -7,11 +7,43 @@ import '../../domain/sources/socket.source.dart';
 
 class SocketRepositoryImpl implements SocketRepository {
   final SocketSource source;
+  final List<Response> _responses = [];
+  String _userId = "";
 
   SocketRepositoryImpl({required this.source});
 
   @override
+  String get userId => _userId;
+
+  @override
+  List<Response> get responses => _responses;
+
+  @override
+  void addResponse(Response response) {
+    _responses.add(response);
+  }
+
+  @override
+  void setUser(String userId) {
+    _userId = userId;
+  }
+
+  @override
   Future<Either<Failure, Response>> connect() {
     return ErrorHandler.async<Response>(source.connect());
+  }
+
+  @override
+  Future<Either<Failure, Stream<Response>>> getMessages() {
+    try {
+      return Future.value(Right(source.getMessages()));
+    } catch (e) {
+      return Future.value(Left(ConnectFailure(e.toString())));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> sendMessage(String userId, String message) {
+    return ErrorHandler.async<void>(source.sendMessage(userId, message));
   }
 }
