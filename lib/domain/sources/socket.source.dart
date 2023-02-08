e2bb8fa182583f35ba0bb20d36f3b4c4ed534f02
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-08 16:36:59 +0800

    feat(source): :sparkles: create get and send message function

--- lib/domain/sources/socket.source.dart
+++ lib/domain/sources/socket.source.dart
@@ -2,4 +2,6 @@ import '../../data/model/response.model.dart';
 
 abstract class SocketSource {
   Future<Response> connect();
+  Stream<Response> getMessages();
+  Future<void> sendMessage(String userId, String message);
 }
