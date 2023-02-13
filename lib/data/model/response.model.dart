59f815f842556ab9e9a59f5b17652ea0160b7312
Joshua Hernandez <joshuamorales401@gmail.com>
2023-02-13 21:36:01 +0800

    refactor(domain): :art: add roomName attribute for response model

--- lib/data/model/response.model.dart
+++ lib/data/model/response.model.dart
@@ -1,16 +1,19 @@
 class Response {
   String userId;
   String message;
+  String roomName;
 
   Response({
     required this.userId,
     required this.message,
+    required this.roomName,
   });
 
   factory Response.fromJson(Map<String, dynamic> json) {
     return Response(
       userId: json["userId"] ?? "",
       message: json["message"] ?? "",
+      roomName: json["roomName"] ?? "",
     );
   }
 }
